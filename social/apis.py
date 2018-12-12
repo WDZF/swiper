3514768abd11014a89fd97441dc9153e12f50cd6
fanfanstl <2094531487@qq.com>
2018-12-12 16:31:00 +0800

    social基本完成

--- social/apis.py
+++ social/apis.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from social.errors import OK, LIKE, SUPER_LIKE, DONT_LIKE
+from social.logic import get_screening_cond, get_match_users, create_swiped
+from user.lib.http import render_json
 from user.models import Profile
 
 
@@ -8,8 +9,8 @@ def get_recommendation(request):
     profile = Profile.objects.get(id=uid)
     # 获取筛选条件
     cond = get_screening_cond(profile)
-
-
+    data = get_match_users(cond)
+    return render_json(data, OK)
 
 
 def match(request):
@@ -17,15 +18,18 @@ def match(request):
 
 
 def like(request):
-    pass
+    data = create_swiped(request, LIKE)
+    return render_json(data, OK)
 
 
 def super_like(request):
-    pass
+    data = create_swiped(request, SUPER_LIKE)
+    return render_json(data, OK)
 
 
 def dont_like(request):
-    pass
+    data = create_swiped(request, DONT_LIKE)
+    return render_json(data, OK)
 
 
 def goback(request):
